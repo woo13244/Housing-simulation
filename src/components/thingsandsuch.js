0038fdbdfb242b3905d7df9de26a052fc794763e
Haley Boyle <haleyjboyle@gmail.com>
2017-09-15 10:11:43 -0700

    added component

---
+++ src/components/thingsandsuch.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ThingsAndSuch extends Component {
+    render() {
+        return (
+            <div>
+                Haley
+            </div>
+        );
+    }
+}
+
+export default ThingsAndSuch ;
